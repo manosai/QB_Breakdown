2a726fe0981ce686831e23c03d84d075dd154f90
manosai <mano.eerabathini@gmail.com>
2013-12-28 14:18:22 -0800

    added homepage html

--- QB_Breakdown_site/QB_Breakdown_site/QB_Breakdown_app/views.py
+++ QB_Breakdown_site/QB_Breakdown_site/QB_Breakdown_app/views.py
@@ -1,2 +1,20 @@
-# nothing here for now
+import json
+import os 
+import requests
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+import boto.sdb
+
+# AWS connection to SimpleDB
+def aws_connect():
+	conn = boto.sdb.connect_to_region('us-east-1',\
+    		aws_access_key_id=os.environ['aws_access_key_id'], \
+            aws_secret_access_key=os.environ['aws_secret_access_key'])
+	return conn
+
+# home endpoint
+def home(request):
+	return render_to_response('home.html', request)
 
