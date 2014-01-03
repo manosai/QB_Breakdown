de5811743caa2ef08df9fbbe7d5341b238802f3f
manosai <mano.eerabathini@gmail.com>
2014-01-02 18:21:49 -0800

    moved css and js to separate media files

---
+++ mysite/mysite/media/js/custom.js
@@ -0,0 +1,161 @@
+$(function() {
+  var arr = [
+  "Carson Palmer", 
+  "Matt Ryan", 
+  "Joe Flacco", 
+  "Thad Lewis",  
+  "Cam Newton",  
+  "Jay Cutler", 
+  "Andy Dalton", 
+  "Jason Campbell",  
+  "Tony Romo",
+  "Peyton Manning",  
+  "Matthew Stafford", 
+  "Matt Flynn",
+  "Matt Schaub",  
+  "Andrew Luck", 
+  "Chad Henne",  
+  "Alex Smith",  
+  "Ryan Tannehil", 
+  "Matt Cassel",
+  "Tom Brady",  
+  "Drew Brees",  
+  "Eli Manning", 
+  "Geno Smith",
+  "Matt McGloin", 
+  "Nick Foles",
+  "Ben Roethlisberger",
+  "Philip Rivers",
+  "Colin Kaepernick", 
+  "Russell Wilson", 
+  "Kellen Clemens", 
+  "Mike Glennon", 
+  "Ryan Fitzpatrick", 
+  "Kirk Cousins", 
+  "Sam Bradford", 
+  "Aaron Rodgers", 
+  "Terelle Pryor", 
+  "Josh McCown"
+  ]; 
+  $( "#qb_names" ).autocomplete({
+    source: arr
+  });
+});
+function keyPressListener(e) { 
+  if (e.keyCode == 13) {
+    loadGraph(); 
+  }
+}
+
+function loadGraph() {
+    // remove landing page content 
+    var el = document.getElementById('landing');
+    if (el != null) {
+      el.parentNode.removeChild(el);
+    }
+
+    // check for empty input 
+    var name = document.getElementsByName("qb_name")[0].value;
+    if (name == "") {
+      alert("You must enter a quarterback's name!");
+      return; 
+    }
+    var children; 
+    $.ajax({
+      'async': false, 
+      'url': 'http://127.0.0.1:8000/get_qb_breakdown', 
+      'type': 'GET', 
+      'data': {
+        'qb': name
+      }, 
+      'success': function(myData) {
+          children = $.parseJSON(myData); 
+      }
+
+    });
+    data = {
+            "name": name, 
+            "type": "quarterback",
+            "children": children
+          };   
+    console.log(children); 
+    drawGraph(data); 
+}
+function getDocHeight() {
+    var D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    );
+}
+
+function drawGraph(data) {
+
+// remove any existing graphs from the page
+d3.selectAll("svg").remove(); 
+
+
+var width = 500,
+    height = getDocHeight() / 1.5;
+ 
+
+var cluster = d3.layout.cluster()
+    .size([height, width - 220]);
+
+var diagonal = d3.svg.diagonal()
+    .projection(function(d) { return [d.y, d.x]; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(100,0)");
+
+// draw the arrowhead
+svg.append("defs").append("marker")
+    .attr("id", "arrowhead")
+    .attr("refX", 9) 
+    .attr("refY", 2)
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 4)
+    .attr("orient", "auto")
+    .append("path")
+        .attr("d", "M 0,0 V 4 L6,2 Z");
+
+var nodes = cluster.nodes(data);
+var links = cluster.links(nodes);
+
+var link = svg.selectAll(".link")
+  .data(links)
+.enter().append("path")
+  .attr("class", "link")
+  .attr("marker-end", "url(#arrowhead)")
+  .attr("d", diagonal);
+
+var node = svg.selectAll(".node")
+  .data(nodes)
+    .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+
+node.append("circle")
+        .attr("r", 4.5);
+
+node.append("text")
+  .attr("dx", function(d) { return d.children ? -8 : 8; })
+  .attr("dy", function(d) { return d.children ? -5 : 3; })
+  .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+  .text(function(d) { return d.name; });
+ 
+
+node.append("text")
+  .attr("dx", function(d) { return d.children ? -8 : 8; })
+  .attr("dy", function(d) { return d.children ? -5: 15; })
+  .text(function(d) { 
+  			if (d.type == 'receiver') {
+  				return "TDs: " + d.touchdowns; 
+  			}
+  });
+d3.select(self.frameElement).style("height", height + "px");
+}
